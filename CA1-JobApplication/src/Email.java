bc3ca6487c0122c89e2159917d8879c27b80674f
eoinpayne <eoinpayne@gmail.com>
2016-02-19 16:23:06 +0000

    .

--- CA1-JobApplication/src/Email.java
+++ CA1-JobApplication/src/Email.java
@@ -2,7 +2,7 @@
 public class Email implements IContact{
 
 	@Override
-	public void SendMessage(String msg_to, String msg_from, String message) {
+	public void sendMessage(String msg_to, String msg_from, String message) {
 		
 		
 		System.out.println("Sending Email");
--- CA1-JobApplication/src/SMS.java
+++ CA1-JobApplication/src/SMS.java
@@ -2,7 +2,7 @@
 public class SMS implements IContact{
 
 	@Override
-	public void SendMessage(String msg_to, String msg_from, String message) {
+	public void sendMessage(String msg_to, String msg_from, String message) {
 		
 		System.out.println("Sending Text");
 	}
